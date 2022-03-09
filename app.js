2c381390f9634737df8db03d434d96a99eec243a
JeffAllison <itsjallison@gmail.com>
2022-03-09 14:36:17 -0800

    added style.backgroundImage to head, middle, bottom

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
+
 // set state for how many times the user changes the head, middle, and bottom
 // set state for all of the character's catchphrases
 
@@ -20,34 +21,42 @@ const catchphrase = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
+    const headSelect = headDropdown.value;
 
     // increment the head change count state
+    headCount++;
 
-    // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
+    headEl.style.backgroundImage = `url(./assets/${headSelect}-head.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
+    const middleSelect = middleDropdown.value;
 
     // increment the middle change count state
+    middleCount++;
 
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url(./assets/${middleSelect}-middle.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-
+    const bottomSelect = bottomDropdown.value;
     // increment the bottom change count state
+    bottomCount++;
 
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    bottomEl.style.backgroundImage = `url(./assets/${bottomSelect}-pants.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
